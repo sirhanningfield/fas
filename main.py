841e99a3f8bc25c0c5eb2a1df05724cb9afbe760
hanan <abdulhannanzr@gmail.com>
2018-11-27 22:43:40 +0800

    added functionality to add class

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import mysql.connector
 
 
 from login import Ui_login
-from admin import Ui_FAS
+from admin import Ui_admin
 
 mydb = mysql.connector.connect(
 	  host="localhost",
@@ -19,7 +19,7 @@ cursor = mydb.cursor(named_tuple=True)
 
 
 
-class Main(QWidget, Ui_login, Ui_FAS):
+class Main(QWidget, Ui_login, Ui_admin):
 
     def __init__(self, parent = None):
         super(Main,self).__init__(parent)
@@ -33,29 +33,65 @@ class Main(QWidget, Ui_login, Ui_FAS):
     def openAminWindow(self):
     	# self.admin_window = Admin(self)
     	self.window = QTabWidget()
-    	self.admin_ui = Ui_FAS()
+    	self.admin_ui = Ui_admin()
     	self.admin_ui.setupUi(self.window)
     	self.window.show()
     	self.admin_ui.add_class_btn.clicked.connect(lambda: self.addClass())
     	self.admin_ui.reg_std_btn.clicked.connect(lambda: self.addStudent())
     	pass
 
-    def addStudent(self):
+    def addStudent(self,):
     	print "Adding student"
     	pass
 
+    def findClass(self,class_id, class_code):
+    	query = "SELECT * FROM classes WHERE class_id = '"+class_id+"' OR class_code = '"+class_code+"'"
+    	cursor.execute(query)
+    	result = cursor.fetchone()
+    	return result
+
+    def addNewClass(self, class_id, class_code, teacher_name):
+    	sql = "INSERT INTO classes (class_id, class_code, teacher_name) VALUES (%s, %s, %s)"
+    	val = (class_id, class_code, teacher_name)
+    	cursor.execute(sql, val)
+    	mydb.commit()
+    	return True
+
+
+
     def addClass(self):
     	print "Add class method"
+    	class_code = str(self.admin_ui.add_class_code.text())
+    	teacher_name = str(self.admin_ui.add_class_teacher.text())
+    	class_id = str(self.admin_ui.add_class_id.text())
+    	credentials = [class_id, class_code, teacher_name]	
 
-    	# check if both fields are filled 
+    	# check if both fields are filled
+    	if self.validateCredentials(credentials,"Please enter all fields") == False :
+    		return 
+    		pass
 
-    	# check if any previous results are available 
+    	# check if any previous results are available
+    	result = self.findClass(class_id, class_code)
 
-    	# if not add the class 
+    	# if not add the class
+    	if result:
+    		self.showErrorMessage("Class already exists")
+    	 	pass
+    	else:
+    		if self.addNewClass(class_id, class_code, teacher_name):
+    			# change status label to class added !
+    			self.showErrorMessage("Class added !")
+    			# clear both text fields
+    			self.admin_ui.add_class_code.setText("")
+    			self.admin_ui.add_class_id.setText("")
+    			self.admin_ui.add_class_teacher.setText("")
+    			pass
+    		else:
+    			self.showErrorMessage("Class could not be added")
+    			return
 
-    	# change status label to class added !
-
-    	# clear both text fields
+    	
     	pass
 
 
@@ -84,13 +120,14 @@ class Main(QWidget, Ui_login, Ui_FAS):
     	h.update(password)
     	return h.hexdigest()
     
-    def validateCredentials(self,username,password):
-   		if len(username) == 0 or len(password) == 0 :   			
-   			self.message = "Please fill in username and password!"
-   			self.showErrorMessage(self.message)
-   			return False
-   		else:
-   			return True
+    def validateCredentials(self,credentials,message):
+    	
+    	for i in range(len(credentials)) :
+    		if (not credentials[i]):
+    			self.showErrorMessage(message)
+    			return False
+    	return True
+	   		
 
     def showErrorMessage(self,message):
    		msg = QMessageBox()
@@ -104,9 +141,10 @@ class Main(QWidget, Ui_login, Ui_FAS):
     	print "login attempt ..."
     	username = str(self.login_ui.username.text())
     	password = str(self.login_ui.password.text())
+    	credentials = [username,password]
 
     	# check if username and password are not null
-    	if self.validateCredentials(username,password) == False :
+    	if self.validateCredentials(credentials,"Please enter username and password") == False :
     		return 
     		pass
  
@@ -121,16 +159,6 @@ class Main(QWidget, Ui_login, Ui_FAS):
 
     	return True
 
-# class Admin(Main):
-# 	"""docstring for Admin"""
-# 	def __init__(self, parent = None):
-# 		super(Admin, self).__init__(parent)
-# 		print "admin class"
-# 		self.parent = parent
-# 		# self.window = QTabWidget()
-#   #   	self.admin_ui = Ui_FAS()
-#   #   	self.admin_ui.setupUi(self.window)
-#   #   	self.window.show()
 						 	
    	
 def main():
